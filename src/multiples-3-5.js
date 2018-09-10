e0011ea4c75754971b5150f38c7965b3882e89b5
Aulden <aulden.harris@gmail.com>
2018-09-10 13:25:17 -0400

    first problem done

--- src/multiples-3-5.js
+++ src/multiples-3-5.js
@@ -2,7 +2,28 @@
 // Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 // Note: If the number is a multiple of both 3 and 5, only count it once.
 function solution(number){
-  // TODO: Create the solution
+  let set = new Set();
+  let sum = 0;
+  for(let i=3; i<number; i+=3){
+    if(!set.has(i)){
+      set.add(i);
+    }
+  }
+
+  for(let i=5; i<number; i+=5){
+    if(!set.has(i)){
+      set.add(i);
+    }
+  }
+
+  set.forEach((val) => {
+    sum += val;
+  });
+
+  if(!number || (typeof number) === "string"){
+    return -1;
+  }
+  return sum;
 }
 
 module.exports = solution;
